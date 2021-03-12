9f56d1ed0a3a61d1a111dd17ba832feb740dd44b
laurens54321 <laurens.hons@gmail.com>
2021-03-12 03:54:14 +0100

    Files I forgot to commit

---
+++ Assets/Scripts/UIScripts/Domain/UIController.cs
@@ -0,0 +1,85 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIController : MonoBehaviour
+{
+    public GameObject camera;
+    private Vector3 cameraOriginPosition;
+    private Vector3 cameraDifference;
+    private bool Drag = false;
+
+    public bool hasMenuOpen = false;
+    
+    private Vector3 dragOrigin;
+    
+    public GameObject BayGameObject;
+    private Bay Bay;
+
+    public int amountOfBays = 1;
+
+    public GameObject MinerStationPrefab;
+
+    public GameObject UICanvas;
+
+    private List<GameObject> MinerStations = new List<GameObject>();
+    
+    void Start()
+    {
+        Bay = BayGameObject.GetComponent<Bay>();
+
+        Vector3 middleOfTheGrid = new Vector3(Bay.gridSize/2 , 0, -1);
+        camera.transform.position = middleOfTheGrid;
+        UICanvas.transform.position = middleOfTheGrid;
+        
+        InstantiateMinerStations();
+    }
+
+    private void InstantiateMinerStations()
+    {
+        for (int i = 0; i < amountOfBays; i++)
+        {
+            Vector2 pos = new Vector2(i * 2f + .5f, -1f);
+            MinerStation minerStation =
+                Instantiate(MinerStationPrefab, pos, Quaternion.identity).GetComponent<MinerStation>();
+            minerStation.setBay(Bay);
+            MinerStations.Add(minerStation.gameObject);
+        }
+    }
+
+    public void moveCamera()
+    {
+        if (hasMenuOpen) return;
+        if (Input.GetMouseButton (0)) {
+            cameraDifference = (Camera.main.ScreenToWorldPoint (Input.mousePosition))- Camera.main.transform.position;
+            if (Drag == false){
+                Drag = true;
+                cameraOriginPosition = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+            }
+        } else {
+            Drag = false;
+        }
+        if (Drag == true){
+            Camera.main.transform.position = new Vector3(Bay.gridSize/2, cameraOriginPosition.y - cameraDifference.y, -1);
+        }
+
+     
+    }
+
+    private void Update()
+    {
+        moveCamera();
+    }
+
+
+    public Bay getBay()
+    {
+        return Bay;
+    }
+    
+    public GameObject getCanvas()
+    {
+        return UICanvas;
+    }
+}
---
+++ Assets/Scripts/UIScripts/Scanner/ScannerSlider.cs
@@ -0,0 +1,96 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+using UnityEngine.Serialization;
+using UnityEngine.UI;
+
+public class ScannerSlider : MonoBehaviour
+{
+    private float accuracy = 4;
+    public float Accuracy
+    {
+        get { return accuracy; }
+        set
+        {
+            slider.maxValue = value;
+            accuracy = value;
+        }
+    }
+
+    private Slider slider;
+    public Text PercentageText;
+    public Text BPSText;
+    public Text BlockTypeText;
+    public Button UpgradeAccuracyButton;
+
+    private float GETSLIDERVAUE;
+    
+    public ScannerController ScannerController;
+
+    private BlockTypes blockType;
+    
+    private void Start()
+    {
+        slider = GetComponentInChildren<Slider>();
+
+        slider.maxValue = accuracy;
+        slider.onValueChanged.AddListener(delegate { ScannerController.updateSliders(); });
+        ScannerController.RegisterScannerSlider(this);
+
+        string gameObjectName = gameObject.name.Remove(0, 6);
+        BlockTypes.TryParse(gameObjectName, out BlockTypes result);
+        blockType = result;
+        BlockTypeText.text = result.ToString();
+
+        if (blockType == BlockTypes.DirtBlock) slider.value = slider.maxValue;
+        
+        UpgradeAccuracyButton.onClick.AddListener(upgradeScannerAccuracy);
+        
+        updatePercentageText();
+    }
+
+    private void Update()
+    {
+        GETSLIDERVAUE = slider.value / accuracy;
+    }
+
+    public void updatePercentageText()
+    {
+        setPercentage( slider.value * 100f / accuracy);
+    }
+    private void setPercentage(float percent)
+    {
+        if (float.IsNaN(percent)) return;
+        PercentageText.text = Math.Round(percent) + "%";
+    }
+
+    
+    public void setBPSText(float percent)
+    {
+        if (float.IsNaN(percent)) BPSText.text = "0 Blocks/sec";
+        else if (percent <= 1/5f) BPSText.text = Math.Round(percent*60, 3) + " Blocks/min";
+        else BPSText.text = Math.Round(percent, 3) + " Blocks/sec";
+        
+    }
+
+    public float getValue()
+    {
+        return slider.value / accuracy;
+    }
+
+    public BlockTypes getBlockType()
+    {
+        return blockType;
+    }
+
+    public void upgradeScannerAccuracy()
+    {
+        accuracy++;
+    }
+
+    
+}
+
+
